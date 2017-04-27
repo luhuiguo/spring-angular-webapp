642d5a52e4c97dbb00a78c9c37dcd7818ec732db
luhuiguo <luhuiguo@gmail.com>
2017-04-27 15:46:01 +0800

    no message

--- src/main/frontend/app/app.module.ts
+++ src/main/frontend/app/app.module.ts
@@ -4,8 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdIconModule, MdButtonModule } from '@angular/material';
-
+/* App Root */
 import { AppComponent } from './app.component';
+/* Feature Modules */
+import { CoreModule } from './core/core.module';
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
